f78bd6eb2c707430feb1f13dfc04dc830d1cdc47
mayank1133 <140825299+mayank1133@users.noreply.github.com>
2023-08-21 09:19:38 +0530

    Add files via upload

---
+++ even or odd(bitwise).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	int a;
+	printf("Enter the Number a:");
+	scanf("%d",&a);
+	if(a>>1==0)
+	{
+		printf("Nummber is Even");
+	}
+	else
+	{
+	
+		printf("Number is Odd");
+	}
+}
